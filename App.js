7e03f22c34a8cfc98bc663fcb16abbc435c5e387
CamillaEva <caha0007@stud.kea.dk>
2026-05-08 12:56:17 +0200

    add book function in component, with search function from API

--- App.js
+++ App.js
@@ -1,92 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, FlatList, Button, Modal } from 'react-native';
 import { useState } from 'react';
-import { database, storage} from './firebase'
+import { database, storage } from './firebase'
 import Login from './components/login';
+import Books from './components/Books';
 
 export default function App() {
- const [modalVisible, setModalVisible] = useState(false)
- const [title, setTitle] = useState('')
- const [description, setDescription] = useState('')
- const [author, setAuthor] = useState('')
- const [startDate, setStartDate] = useState('')
- const [endDate, setEndDate] = useState('')
- const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [author, setAuthor] = useState('')
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  return isLoggedIn ?(
+  return isLoggedIn ? (
     <View style={styles.container}>
       <Text>Welcome to Mybooks</Text>
       <Text>A place where you can handle your books</Text>
 
-      <Button title='Add book' onPress={() => setModalVisible(true)}/>
+      <Button title='Add book' onPress={() => setModalVisible(true)} />
 
-      <Modal visible={modalVisible}>
-        <View style={styles.container}>
-           <Text style={styles.header}>Add a new book</Text>
-
-          <TextInput
-            placeholder='Book title'
-            value={title}
-            onChangeText={setTitle}
-            style={styles.input}
-          />
-
-          <TextInput
-            placeholder='Book description'
-            value={description}
-            onChangeText={setDescription}
-            style={styles.input}
-          />
-
-          <TextInput
-            placeholder='Author'
-            value={author}
-            onChangeText={setAuthor}
-            style={styles.input}
-          />
-
-          <TextInput 
-          placeholder='Start date'
-          value={startDate}
-          onChangeText={setStartDate}
-          style={styles.input}
-          />
-
-          <TextInput 
-          placeholder='End date'
-          value={endDate}
-          onChangeText={setEndDate}
-          style={styles.input}
-          />
-
-           <Button 
-            title='Save book' 
-            onPress={() => {
-              console.log(title, description, author, startDate, endDate);
-
-              setTitle('')
-              setDescription('')
-              setAuthor('')
-              setStartDate('')
-              setEndDate('')
-            
-              setModalVisible(false);
-            }} 
-          />
-
-          <Button 
-            title="Cancel" 
-            onPress={() => setModalVisible(false)} 
-            color="red"
-          />
-        </View>
-      </Modal>
+      <Books
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
 
       <StatusBar style="auto" />
     </View>
-  ): (
-  <Login onLogin={() => setIsLoggedIn(true)}></Login>
-);}
+  ) : (
+    <Login onLogin={() => setIsLoggedIn(true)}></Login>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
